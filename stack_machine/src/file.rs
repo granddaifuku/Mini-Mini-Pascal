c718962b5cfa546fe706bb9887eaada5c3e3ff32
granddaifuku <daifukuformac@gmail.com>
2021-12-12 00:55:43 +0900

    feat: impl file read

---
+++ stack_machine/src/file.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[allow(dead_code)]
+pub fn read_file(path: String) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let mut inputs: Vec<String> = Vec::new();
+    for res in BufReader::new(File::open(path)?).lines() {
+        let line = res?;
+        inputs.push(line);
+    }
+
+    Ok(inputs)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn read_file_success() {
+        let mut want: Vec<String> = Vec::new();
+        want.push(String::from("a"));
+        want.push(String::from("b c"));
+        want.push(String::from("d123"));
+        let got = read_file(String::from("./tests/read_file_normal.txt"));
+        if got.is_err() {
+            panic!("Failed to open file");
+        }
+        assert_eq!(want, got.unwrap());
+    }
+
+    #[test]
+    fn read_file_fail() {
+        let got = read_file(String::from("./tests/not_exists_file_example.txt"));
+        assert!(got.is_err());
+    }
+}
--- stack_machine/src/main.rs
+++ stack_machine/src/main.rs
@@ -1,3 +1,11 @@
+mod file;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Number of args should be 2");
+    }
+    let _ = file::read_file(args[1].clone());
 }
